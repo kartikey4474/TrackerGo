bb256dc189c5b0fc86b3003558c842ca66ef9cd9
Kartikey singh <kartikey4474>
2021-09-18 10:13:59 +0530

    commit upto chart bars

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/transaction.dart';
+import 'package:flutter_application_1/Widgets/transaction_list.dart';
+import './Widgets/transaction_list.dart';
+import 'Widgets/chart.dart';
+import 'Widgets/new_transactions.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,43 +11,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        fontFamily: 'Quicksand',
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  List<Transaction> transactions = [
-    Transaction(id: 't1', title: 'shoe', amount: 99.99, date: DateTime.now())
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    // Transaction(id: 't1', title: 'shoe', amount: 99.99, date: DateTime.now()),
+    // Transaction(
+    //     id: 't2', title: 'carry bag', amount: 299.99, date: DateTime.now()),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  void _addTransaction(String txtitle, double txamount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txtitle,
+        amount: txamount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            highlightColor: Colors.purple,
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
       ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('device area'),
-                elevation: 5,
-              ),
-            ),
-            Container(
-              width: 100,
-              child: Card(
-                color: Colors.red,
-                elevation: 5,
-                child: Text('hy yaa'),
-              ),
-            ),
-          ]),
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Chart(_recentTransactions),
+              TransactionList(_userTransaction),
+            ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _startAddNewTransaction(context),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
